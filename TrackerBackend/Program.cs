d1ec4a6250b9584f430001a46a1daf2ca525b882
dextrinne <167472397+dextrinne@users.noreply.github.com>
2026-07-15 08:14:40 +0300

    Раскомментирование Swagger

--- TrackerBackend/Program.cs
+++ TrackerBackend/Program.cs
@@ -72,11 +72,8 @@ using (var scope = app.Services.CreateScope())
     }
 }
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // app.UseHttpsRedirection();
 app.UseCors("VueClient");
